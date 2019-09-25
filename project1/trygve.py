7865a41e02949b3d491397dedb1c080a29a974c2
Trygve Leithe Svalheim <svalheim@obelisk.local>
2019-09-25 12:43:09 +0200

    Update

--- project1/trygve.py
+++ project1/trygve.py
@@ -6,12 +6,13 @@ from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import Lasso, LinearRegression
+from sklearn.linear_model import Lasso, LinearRegression, Ridge
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 import numpy as np
 import numpy.linalg as la
-from random import random, seed
+from random import random, seed, shuffle
 import pandas as pd
-from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+
 # Seed parameters
 seed = 42069
 np.random.seed(seed)
@@ -101,8 +102,8 @@ def var2(y_pred):
     variance = np.mean(np.var(y_pred,axis=1,keepdims=True))
     return variance
 
-# Optional plotting step
-def plotstuff(y_train_pred,y_test_pred):
+# Plot surface plots
+def plotstuff(y_train_pred,y_test_pred,error):
     # Plotting parameters
     fig = plt.figure()
     ax1 = fig.add_subplot(111, projection = '3d')
@@ -116,26 +117,27 @@ def plotstuff(y_train_pred,y_test_pred):
     
     # Plot training data fit
     ax1.scatter(data_train[0],data_train[1],y_train_pred,alpha=1, s=1, color="C0", label="Training data")
-    ax1.scatter(data_test[0],data_test[1],y_test_pred,alpha=1, s=1, color="C1", label="Test data")
+    ax1.scatter(data_test[0],data_test[1],y_test_pred,alpha=1, s=1, color="C1", label=r"Test data - $R^2 = %.3f$" %error)
     
     plt.legend()
     plt.show()
 
-
-def error(errors,degrees,filename,lamb,p,printerror):
-    if printerror==True:
-        for i, degree in enumerate(degrees):
+# Print errors and plot bias-variance
+def error(errors, degrees, lamb, p):
+    for i, degree in enumerate(degrees):
             
-            print(" ")
-            print("Polynomial of degree: ", i)
-            print("Total error on training data: ")
-            print("MSE: %.5f,  R^2: %.5f" %(errors[0,0,i], errors[1,0,i]))
-
-            print("Total error on test data: ")
-            print("MSE: %.5f,  R^2: %.5f" %(errors[0,1,i], errors[1,1,i]))
+        print(" ")
+        print("Polynomial of degree: ", i)
+        print("Total error on training data: ")
+        print("MSE: %.5f,  R^2: %.5f" %(errors[0,0,i], errors[1,0,i]))
+        
+        print("Total error on test data: ")
+        print("MSE: %.5f,  R^2: %.5f" %(errors[0,1,i], errors[1,1,i]))
                 
     if len(degrees)<2:
-        raise ValueError('Too few polynomial degrees for bias-variance plot')
+        print('Too few polynomial degrees for bias-variance plot')
+        return False
+    
     method_name = str(method.__name__)
 
     if method_name!="OLS":
@@ -150,16 +152,16 @@ def error(errors,degrees,filename,lamb,p,printerror):
     plt.plot(degrees, errors[0,0,:],label=lambd+"Training data MSE",color="C"+str(p)) # Test data
     plt.plot(degrees, errors[0,1,:],label=lambd+"Test data MSE",color="C"+str(p),alpha=alpha) # Test data
     
-    # Bias
-    plt.plot(degrees, errors[2,0,:],label=lambd+"Training data - bias",color="C"+str(p+1), linestyle=":") # Training data
+    ## Bias
+    #plt.plot(degrees, errors[2,0,:],label=lambd+"Training data - bias",color="C"+str(p+1), linestyle=":") # Training data
     plt.plot(degrees, errors[2,1,:],label=lambd+"Test data - bias",color="C"+str(p+1), linestyle=":", alpha=alpha) # Training data
-    
-    # Variance
-    plt.plot(degrees, errors[3,0,:],label=lambd+"Training data - variance",linestyle="--",color="C"+str(p+2))
+    #
+    ## Variance
+    #plt.plot(degrees, errors[3,0,:],label=lambd+"Training data - variance",linestyle="--",color="C"+str(p+2))
     plt.plot(degrees, errors[3,1,:],label=lambd+"Test data - variance",linestyle="--",color="C"+str(p+2),alpha=alpha) # Test data
-    
-    # Bias + Variance
-    plt.plot(degrees, errors[2,0,:]+errors[3,0,:],label=r"Training data - Variance + Bias",linestyle="-.",color="C"+str(p+3)) # Test data
+    #
+    ## Bias + Variance
+    #plt.plot(degrees, errors[2,0,:]+errors[3,0,:],label=r"Training data - Variance + Bias",linestyle="-.",color="C"+str(p+3)) # Test data
     plt.plot(degrees, errors[2,1,:]+errors[3,1,:],label=r"Test data - Variance + Bias",linestyle="-.",color="C"+str(p+3),alpha=alpha) # Test data
     
     p += 1 #Color cycle for each lambda
@@ -171,7 +173,7 @@ def error(errors,degrees,filename,lamb,p,printerror):
     #plt.ylim([0,0.1])
     plt.legend()
 
-if True:
+if False: # Use Franke function
     # Make data.
     N = 20
     x = np.linspace(0, 1, N)
@@ -186,40 +188,69 @@ if True:
     b = FrankeFunction(x_, y_) + np.random.normal(size=x_.shape)*noise # Franke function with optional gaussian noise
     
     y = pd.DataFrame(b.ravel().T)
-else:
-    b = imread("../MachineLearning/doc/Projects/2019/Project1/Datafiles/SRTM_data_Norway_2.tif")[100:120,100:120]
+else: # Using real terrain data
+    downsample = 60
+    b = imread("../MachineLearning/doc/Projects/2019/Project1/Datafiles/SRTM_data_Norway_2.tif")[:-1:downsample,:-1:downsample]
     length = b.shape[0]
     width = b.shape[1]
+    print(length,width)
     x_,y_ = np.meshgrid(range(width), range(length))
     data = np.c_[(x_.ravel()).T,(y_.ravel()).T]
     data = pd.DataFrame(data)
     y = pd.DataFrame(b.ravel().T)
 
-test_size=0.2
-k = 100; 
+# KFOLD
+kfolds = 5
+test_size=1/kfolds
 degrees = range(15)
+ylen = y.shape[0]
+testsize = int(ylen/kfolds)
+trainsize = ylen-testsize
+
+# RESAMPLING
+#k = 100
+#test_size=0.2
+#degrees = range(15)
+#ylen = y.shape[0]
+#testsize = int(ylen*test_size)
+#trainsize = ylen-testsize
+
 method = OLS; lambdas=[0]
-#method = lasso; lambdas=[0.1]; #lambdas=[0.001, 0.01, 0.1, 0.3]
-#method = ridge; lambdas=[0.1]
+method = ridge; lambdas=[0.0001, 0.001, 0.01, 0.1, 0.3]
+method = lasso; lambdas=[0.0001, 0.001, 0.01, 0.1, 0.3]
 
 # Something wrong with the bias in OLS? Ridge is weird too.
 def splitdata(X,y):
     X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=test_size, shuffle=True)
     return X_train, X_test, y_train, y_test 
 
-y_train_size = int((1-test_size)*y.shape[0])
-y_test_size = int((test_size)*y.shape[0])
-
-y_train = np.empty((y_train_size,k))
-y_test = np.empty((y_test_size,k))
-y_train_pred = np.empty((y_train_size,k))
-y_test_pred = np.empty((y_test_size,k))
+def kfold(X,nsplits):
+    train_idx = np.empty((nsplits, trainsize))
+    test_idx = np.empty((nsplits, testsize))
+    X=list(range(ylen))
+    shuffle(X)
+    for k in range(nsplits):
+        X_duplicate = X.copy()
+        test_idx[k,:] = X_duplicate[testsize*k:testsize*(k+1)]
+        del X_duplicate[testsize*k:testsize*(k+1)]
+        train_idx[k,:] = X_duplicate
+    return train_idx.astype(int), test_idx.astype(int)
 
 for lamb in lambdas: # Iterate over all hyperparemeters lambda
     k_errors = np.zeros((4, 2, len(degrees))) # Initializing error array
     for d, degree in enumerate(degrees): # Iterate over polynomial complexity
-        for k in range(k): # K-fold test. Do it k times and check errors.
-            data_train, data_test, y_tra, y_tes = splitdata(data,y) # Sample data
+        y_train = np.empty((trainsize,kfolds))
+        y_test = np.empty((testsize,kfolds))
+        y_train_pred = np.empty((trainsize,kfolds))
+        y_test_pred = np.empty((testsize,kfolds))
+        train_idx,test_idx = kfold(data,kfolds) # Kfold splits
+
+        for k in range(kfolds): # K-fold test. Do it k times and check errors.
+            # K FOLD STUFF
+            data_train, data_test,y_tra,y_tes = data.iloc[train_idx[k,:]],data.iloc[test_idx[k,:]] ,y.iloc[train_idx[k,:]], y.iloc[test_idx[k,:]]
+            # RESAMPLING (OLD)
+            #data_train, data_test, y_tra, y_tes = splitdata(data,y) # Sample data
+
             X_train = design(data_train,degree) # Design matrix for training data
             X_test = design(data_test,degree) # Design matrix for training data
             
@@ -228,7 +259,6 @@ for lamb in lambdas: # Iterate over all hyperparemeters lambda
 
             y_train_pred[:,k], y_test_pred[:,k], beta = method(X_train,y_train[:,k],X_test,y_test[:,k],lamb)
 
-            
         # Save training errors
         k_errors[0, 0, d] = mse(y_train, y_train_pred)
         k_errors[1, 0, d] = R_squared(y_train, y_train_pred)
@@ -241,11 +271,9 @@ for lamb in lambdas: # Iterate over all hyperparemeters lambda
         k_errors[2, 1, d] = bias(y_test, y_test_pred)
         k_errors[3, 1, d] = var2(y_test_pred)
             
-    filename=str(method)+str(lamb)+".png"
-    error(k_errors, degrees, filename, lamb, p, True)
-
+    error(k_errors, degrees, lamb, p) # Bias variance plot and errors
+    plotstuff(y_train_pred[:,-1],y_test_pred[:,-1], k_errors[1,1,-1]) # Plot surfaces
+    
+#plt.savefig(str(method.__name__)+".png") # Save whatever figure youre plotting
 plt.show()
-plt.savefig(str(method.__name__)+".png")
-#plotstuff(y_train_pred[:,-1],y_test_pred[:,-1])
-
 
