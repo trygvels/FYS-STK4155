0c3011426092670182d9322b45a56d0b1f2a6217
Trygve Leithe Svalheim <svalheim@obelisk.local>
2019-09-29 21:24:37 +0200

    Sunday update

--- project1/trygve.py
+++ project1/trygve.py
@@ -12,12 +12,13 @@ import numpy as np
 import numpy.linalg as la
 from random import random, seed, shuffle
 import pandas as pd
+plt.style.use(u"~/.matplotlib/stylelib/trygveplot_astro.mplstyle")
 
 # Seed parameters
-seed = 42069
-np.random.seed(seed)
+seednbr = 42069
+np.random.seed(seednbr)
+seed(seednbr)
 p = 0 # For color cycle
-
 
 # Franke Function
 def FrankeFunction(x,y):
@@ -36,51 +37,52 @@ def design(data, degree):
 # Applying SVD to design matrix A and function b
 def svd_solver(A,b):
     U, sigma, VT = la.svd(A)  # Solve SVD system
-    Sigma = np.zeros(A.shape) # Initialize empty Sigma array
+    Sigma = np.zeros(A.shape) # Initialize zeros Sigma array
     diags = np.diag(sigma) # Width of Sigma array
     hehe = len(diags)
     Sigma[:hehe,:hehe] = diags # Fill diagonal elements in top half
     Sigma_pinv = np.zeros(A.shape).T    
     Sigma_pinv[:hehe,:hehe] = np.diag(1/sigma[:hehe])
     Sigma_pinv.round(3)
-
     x_svd = VT.T.dot(Sigma_pinv).dot(U.T).dot(b) # Solve for x
     return x_svd
 
 def OLS(X_train, y_train, X_test, y_test, lamb):
-    beta = svd_solver(X_train,y_train)  # Solve for x using ONLY training data
-    y_train_pred = X_train.dot(beta).ravel() #reshape(y_tra.shape) # Check how beta matches training data
-    
-    y_test_pred = X_test.dot(beta).ravel() #.reshape(y_test.shape) # Test (old) x on test data
-    #fit = LinearRegression().fit(X_train, y_train)
-    #y_train_pred = fit.predict(X_train).reshape(y_train.shape)
-    return y_train_pred, y_test_pred, beta
+    if True:
+        beta = svd_solver(X_train,y_train)  # Solve for x using ONLY training data
+        y_train_pred = X_train.dot(beta).ravel() #reshape(y_tra.shape) # Check how beta matches training data
+        y_test_pred = X_test.dot(beta).ravel() #.reshape(y_test.shape) # Test (old) x on test data
+        intercept = 0
+    else:
+        fit = LinearRegression().fit(X_train, y_train)
+        beta = fit.coef_
+        intercept = fit.intercept_
+        y_train_pred = fit.predict(X_train).reshape(y_train.shape)
+        y_test_pred = fit.predict(X_test).reshape(y_test.shape)
+    return y_train_pred, y_test_pred, beta,intercept
     
 def ridge(X_train,y_train,X_test,y_test, lamb):
-    X_train_reduced = X_train[0] + X_train - np.mean(X_train, axis=0) # Zero center training data
-    y_train_reduced = y_train - np.mean(y_train)
-
-    beta = (1-lamb)**-1*svd_solver(X_train_reduced, y_train_reduced) 
-
-    y_train_pred = X_train_reduced.dot(beta).reshape(y_train_reduced.shape) # Check how beta matches training data                
-    y_train_pred += np.mean(y_train)
-
-    #fit = Ridge(alpha=lamb).fit(X_train, y_train)
-    #y_train_pred = fit.predict(X_train).reshape(y_train.shape)
-
-    X_test_reduced = X_test - np.mean(X_train, axis=0)
-    y_test_pred = X_test_reduced.dot(beta).reshape(y_test.shape) # Test (old) x on test data                  
-
-    #y_test_pred = fit.predict(X_test).reshape(y_test.shape)
-    y_test_pred += np.mean(y_train)
-    return y_train_pred, y_test_pred, beta
+    if True: # Doing the algebra
+        I = np.identity(X_train.shape[1])        
+        beta = np.dot(np.dot(np.linalg.inv(np.dot(X_train.T, X_train) + lamb * I), X_train.T), y_train)
+        y_train_pred = X_train.dot(beta).reshape(y_train.shape)
+        y_test_pred = X_test.dot(beta).reshape(y_test.shape)
+        intercept = 0
+    else: # SKlearn for comparison
+        fit = Ridge(alpha=lamb).fit(X_train, y_train)
+        beta = fit.coef_
+        intercept = fit.intercept_
+        y_train_pred = fit.predict(X_train).reshape(y_train.shape)
+        y_test_pred = fit.predict(X_test).reshape(y_test.shape)
+    return y_train_pred, y_test_pred, beta, intercept
 
 def lasso(X_train, y_train, X_test,y_test,lamb):
     fit = Lasso(alpha=lamb).fit(X_train, y_train)
     beta = fit.coef_
+    intercept = fit.intercept_
     y_train_pred = fit.predict(X_train).reshape(y_train.shape)
     y_test_pred = fit.predict(X_test).reshape(y_test.shape)
-    return y_train_pred, y_test_pred, beta
+    return y_train_pred, y_test_pred, beta,intercept
 
 
 # Mean squared error
@@ -103,31 +105,47 @@ def var2(y_pred):
     return variance
 
 # Plot surface plots
-def plotstuff(y_train_pred,y_test_pred,error):
+def plotterrain(y_train_pred,y_test_pred,error, besty,save,lamb):
     # Plotting parameters
     fig = plt.figure()
     ax1 = fig.add_subplot(111, projection = '3d')
-    #ax1.set_zlim3d(-0.2,1.2)
-    #ax1.set_zlim3d(-100,400)
     
-    # Plot franke function
-    surf = ax1.plot_surface(x_, y_, b, alpha=0.5, cmap=cm.coolwarm,label="Franke function") # Plot franke function
+    ax1.set_zlim3d(-0.2,1.2) # Zlimit
+    ax1.view_init(5,60) # Optimal viewing angle
+    
+    # Remove background
+    ax1.xaxis.pane.fill = False
+    ax1.yaxis.pane.fill = False
+    ax1.zaxis.pane.fill = False
+    ax1.xaxis.pane.set_edgecolor('w')
+    ax1.yaxis.pane.set_edgecolor('w')
+    ax1.zaxis.pane.set_edgecolor('w')
+    
+    # Plot franke function #alpha=0.5
+    surf = ax1.plot_surface(x_, y_, b, alpha=0.3, cmap=cm.coolwarm,label=r"Franke function $N(0,%.2f)$" %noise)
     surf._facecolors2d=surf._facecolors3d # Bugfix for legend
     surf._edgecolors2d=surf._edgecolors3d
-    
+    #surf1 = ax1.plot_surface(x_, y_, besty.reshape(b.shape), alpha=.5, cmap=cm.BrBG, label="Best fit "+method.__name__+" P"+str(degree)+" $\lambda = %.4f$" %lamb)
+    #surf1._facecolors2d=surf._facecolors3d # Bugfix for legend
+    #surf1._edgecolors2d=surf._edgecolors3d
+
+
     # Plot training data fit
-    ax1.scatter(data_train[0],data_train[1],y_train_pred,alpha=1, s=1, color="C0", label="Training data")
-    ax1.scatter(data_test[0],data_test[1],y_test_pred,alpha=1, s=1, color="C1", label=r"Test data - $R^2 = %.3f$" %error)
-    
+    ax1.scatter(x_,y_,besty,alpha=1, s=1, color="C1", label="Best fit "+method.__name__+" P"+str(degree)+" $\lambda = %.4f$" %lamb)
+    #ax1.scatter(data_train[0],data_train[1],y_train_pred[:,-1],alpha=1, s=1, color="C1", label="Training data")
+    #ax1.scatter(data_test[0],data_test[1],y_test_pred[:,-1],alpha=1, s=1, color="C0", label=r"Test data - $R^2 = %.3f$" %error)
     plt.legend()
+    plt.tight_layout()
+    if save==True:
+        plt.savefig(str(method.__name__)+"_L"+str(lamb)+"_P"+str(degree)+"_"+datatype+".png",bbox_inches = 'tight',pad_inches = 0) # Save whatever figure youre plotting
+        #plt.savefig("franke.png",bbox_inches = 'tight',pad_inches = 0)
     plt.show()
 
 # Print errors and plot bias-variance
 def error(errors, degrees, lamb, p):
     for i, degree in enumerate(degrees):
-            
         print(" ")
-        print("Polynomial of degree: ", i)
+        print("Polynomial of degree: ", degree)
         print("Total error on training data: ")
         print("MSE: %.5f,  R^2: %.5f" %(errors[0,0,i], errors[1,0,i]))
         
@@ -149,31 +167,31 @@ def error(errors, degrees, lamb, p):
     alpha = 0.5
 
     # Error
-    plt.plot(degrees, errors[0,0,:],label=lambd+"Training data MSE",color="C"+str(p)) # Test data
-    plt.plot(degrees, errors[0,1,:],label=lambd+"Test data MSE",color="C"+str(p),alpha=alpha) # Test data
+    plt.plot(degrees, errors[0,0,:],label=lambd+"MSE    -    Training data",color="C"+str(p)) # Test data
+    plt.plot(degrees, errors[0,1,:],label=lambd+"MSE    -    Test data",color="C"+str(p),alpha=alpha) # Test data
     
     ## Bias
     #plt.plot(degrees, errors[2,0,:],label=lambd+"Training data - bias",color="C"+str(p+1), linestyle=":") # Training data
-    plt.plot(degrees, errors[2,1,:],label=lambd+"Test data - bias",color="C"+str(p+1), linestyle=":", alpha=alpha) # Training data
+    plt.plot(degrees, errors[2,1,:],label=lambd+"Bias     -    Test data",color="C"+str(p+1), linestyle=":", alpha=alpha) # Training data
     #
     ## Variance
     #plt.plot(degrees, errors[3,0,:],label=lambd+"Training data - variance",linestyle="--",color="C"+str(p+2))
-    plt.plot(degrees, errors[3,1,:],label=lambd+"Test data - variance",linestyle="--",color="C"+str(p+2),alpha=alpha) # Test data
+    plt.plot(degrees, errors[3,1,:],label=lambd+"Variance - Test data",linestyle="--",color="C"+str(p+2),alpha=alpha) # Test data
     #
     ## Bias + Variance
     #plt.plot(degrees, errors[2,0,:]+errors[3,0,:],label=r"Training data - Variance + Bias",linestyle="-.",color="C"+str(p+3)) # Test data
-    plt.plot(degrees, errors[2,1,:]+errors[3,1,:],label=r"Test data - Variance + Bias",linestyle="-.",color="C"+str(p+3),alpha=alpha) # Test data
+    #plt.plot(degrees, errors[2,1,:]+errors[3,1,:],label=r"Test data - Variance + Bias",linestyle="-.",color="C"+str(p+3),alpha=alpha) # Test data
     
     p += 1 #Color cycle for each lambda
     #plt.title("MSE for training and test data")
     plt.xlabel("Polynomial degree")
     plt.ylabel("Prediction error")
     plt.title(method_name)
-    plt.xlim([0,len(degrees)])
-    #plt.ylim([0,0.1])
+    plt.xlim([0,len(degrees)-1])
+    plt.ylim([0,errors[0,0,0]*1.5])
     plt.legend()
 
-if False: # Use Franke function
+if True: # Use Franke function
     # Make data.
     N = 20
     x = np.linspace(0, 1, N)
@@ -184,13 +202,16 @@ if False: # Use Franke function
     data = pd.DataFrame(data)
 
     # Create and transform franke function data
-    noise = 0.1 # Amount of noise
+    noise = 0.05 # Amount of noise
     b = FrankeFunction(x_, y_) + np.random.normal(size=x_.shape)*noise # Franke function with optional gaussian noise
-    
     y = pd.DataFrame(b.ravel().T)
+    datatype = "franke_N"+str(noise)
 else: # Using real terrain data
     downsample = 60
     b = imread("../MachineLearning/doc/Projects/2019/Project1/Datafiles/SRTM_data_Norway_2.tif")[:-1:downsample,:-1:downsample]
+    #b = imread("../MachineLearning/doc/Projects/2019/Project1/Datafiles/SRTM_data_Norway_2.tif")[100:120,100:120]
+    b -= np.min(b)
+    b = b/np.max(b)
     length = b.shape[0]
     width = b.shape[1]
     print(length,width)
@@ -198,26 +219,27 @@ else: # Using real terrain data
     data = np.c_[(x_.ravel()).T,(y_.ravel()).T]
     data = pd.DataFrame(data)
     y = pd.DataFrame(b.ravel().T)
-
+    datatype = "terrain"
 # KFOLD
 kfolds = 5
 test_size=1/kfolds
-degrees = range(15)
+degrees = [5]#Range(15)
 ylen = y.shape[0]
 testsize = int(ylen/kfolds)
 trainsize = ylen-testsize
 
 # RESAMPLING
-#k = 100
+#kfolds = 100
 #test_size=0.2
-#degrees = range(15)
+#degrees = [5]
 #ylen = y.shape[0]
 #testsize = int(ylen*test_size)
 #trainsize = ylen-testsize
 
 method = OLS; lambdas=[0]
-method = ridge; lambdas=[0.0001, 0.001, 0.01, 0.1, 0.3]
-method = lasso; lambdas=[0.0001, 0.001, 0.01, 0.1, 0.3]
+method = ridge; lambdas=[0.0001, 0.001, 0.01, 0.1, 0.3];# lambdas=[0.0001]
+method = lasso; lambdas=[0.0001, 0.001, 0.01, 0.1, 0.3];# lambdas=[0.0001]
+#lambdas=np.logspace(-5,0,1000)
 
 # Something wrong with the bias in OLS? Ridge is weird too.
 def splitdata(X,y):
@@ -225,8 +247,8 @@ def splitdata(X,y):
     return X_train, X_test, y_train, y_test 
 
 def kfold(X,nsplits):
-    train_idx = np.empty((nsplits, trainsize))
-    test_idx = np.empty((nsplits, testsize))
+    train_idx = np.zeros((nsplits, trainsize))
+    test_idx = np.zeros((nsplits, testsize))
     X=list(range(ylen))
     shuffle(X)
     for k in range(nsplits):
@@ -236,18 +258,25 @@ def kfold(X,nsplits):
         train_idx[k,:] = X_duplicate
     return train_idx.astype(int), test_idx.astype(int)
 
-for lamb in lambdas: # Iterate over all hyperparemeters lambda
+terms = int((degrees[0]+1)*(degrees[0]+2)/2)
+bestbetas = np.zeros((terms,len(lambdas),2))
+for l, lamb in enumerate(lambdas): # Iterate over all hyperparemeters lambda
     k_errors = np.zeros((4, 2, len(degrees))) # Initializing error array
     for d, degree in enumerate(degrees): # Iterate over polynomial complexity
-        y_train = np.empty((trainsize,kfolds))
-        y_test = np.empty((testsize,kfolds))
-        y_train_pred = np.empty((trainsize,kfolds))
-        y_test_pred = np.empty((testsize,kfolds))
-        train_idx,test_idx = kfold(data,kfolds) # Kfold splits
+        y_train = np.zeros((trainsize,kfolds))
+        y_test = np.zeros((testsize,kfolds))
+        y_train_pred = np.zeros((trainsize,kfolds))
+        y_test_pred = np.zeros((testsize,kfolds))
+        intercept = np.zeros(kfolds)
+        terms = int((degree+1)*(degree+2)/2)
+        betas = np.zeros((terms,kfolds))
 
+        train_idx, test_idx = kfold(data,kfolds) # Kfold splits
         for k in range(kfolds): # K-fold test. Do it k times and check errors.
             # K FOLD STUFF
-            data_train, data_test,y_tra,y_tes = data.iloc[train_idx[k,:]],data.iloc[test_idx[k,:]] ,y.iloc[train_idx[k,:]], y.iloc[test_idx[k,:]]
+            data_train, data_test,y_tra,y_tes = data.iloc[train_idx[k,:]],\
+                data.iloc[test_idx[k,:]] ,y.iloc[train_idx[k,:]], y.iloc[test_idx[k,:]]
+
             # RESAMPLING (OLD)
             #data_train, data_test, y_tra, y_tes = splitdata(data,y) # Sample data
 
@@ -257,8 +286,19 @@ for lamb in lambdas: # Iterate over all hyperparemeters lambda
             y_train[:,k] = y_tra[0] # Index stuff
             y_test[:,k] = y_tes[0]
 
-            y_train_pred[:,k], y_test_pred[:,k], beta = method(X_train,y_train[:,k],X_test,y_test[:,k],lamb)
+            y_train_pred[:,k], y_test_pred[:,k], betas[:,k], intercept[k] = method(X_train,y_train[:,k],\
+                                                                     X_test,y_test[:,k],lamb)
 
+
+        # calculate averaged over kfolds (Best fit)
+        bestbetas[:,l,0] = np.mean(betas,axis=1)
+
+        
+        bestbetas[:,l,1] = np.var(betas,axis=1)
+
+        besty = design(data, degree).dot(bestbetas[:,l,0])+np.mean(intercept)
+        mse(y.values, besty)
+        
         # Save training errors
         k_errors[0, 0, d] = mse(y_train, y_train_pred)
         k_errors[1, 0, d] = R_squared(y_train, y_train_pred)
@@ -270,10 +310,26 @@ for lamb in lambdas: # Iterate over all hyperparemeters lambda
         k_errors[1, 1, d] = R_squared(y_test, y_test_pred)
         k_errors[2, 1, d] = bias(y_test, y_test_pred)
         k_errors[3, 1, d] = var2(y_test_pred)
-            
-    error(k_errors, degrees, lamb, p) # Bias variance plot and errors
-    plotstuff(y_train_pred[:,-1],y_test_pred[:,-1], k_errors[1,1,-1]) # Plot surfaces
-    
-#plt.savefig(str(method.__name__)+".png") # Save whatever figure youre plotting
+
+        plotterrain(y_train_pred,y_test_pred, k_errors[1,1,d],besty,True, lamb) # Plot surfaces
+    #error(k_errors, degrees, lamb, p) # Bias variance plot and errors
+"""
+alpha = 1.0
+for i in range(14):
+    if i>6:
+        alpha = 0.5
+    plt.errorbar(lambdas, bestbetas[i,:,0],yerr=np.sqrt(bestbetas[i,:,1]), errorevery=20,label=r"$\beta_{%i}$"%i,alpha=alpha)
+    plt.xscale("log")
+plt.legend(loc="right")
+plt.xlabel(r"$\beta$")
+plt.ylabel(r"$\lambda$")
+plt.tight_layout()
+plt.savefig(method.__name__+"_lambdas.png",bbox_inches = 'tight',pad_inches = 0)
 plt.show()
+"""
+#plt.savefig("BV_"+str(method.__name__)+"_L"+str(lamb)+"_"+datatype+".png",bbox_inches = 'tight',pad_inches = 0) # Save whatever figure youre plotting
+#plt.show()    
+
+
+
 
